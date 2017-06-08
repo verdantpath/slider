bee0c7f6615e45966dfaf8b53479046c7db8b5ad
Matt <m_dittmann@yahoo.com>
2017-06-07 21:52:48 -0700

    update js

--- js/slider.js
+++ js/slider.js
@@ -2,4 +2,6 @@ $('.slider').each(function() {
   var $this = $(this);
   var $group = $this.find('.slide-group');
   var $slides = $this.find('.slide');
+  var buttonArray = [];
+  var currentIndex = 0;
 });
